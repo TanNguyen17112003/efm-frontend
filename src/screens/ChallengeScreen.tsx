cdb28c5d34cb740316117712009a2c24e6f911cd
TanNguyen <duytan17112003@gmail.com>
2024-03-17 00:38:28 +0700

    feat(challenge): add challenge and goal screen, drawer component

--- src/screens/ChallengeScreen.tsx
+++ src/screens/ChallengeScreen.tsx
@@ -1,14 +1,16 @@
 import { Box, Text, FlatList, View, Input, Pressable, Icon, Progress, Image, ScrollView } from "native-base"
 import { PlusCircleIcon, BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/solid"
+import { Drawer } from "@components";
 import tw from 'twrnc';
 export const ChallengeScreen = () => {
+    const formatter = new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit' });
     const challengeList = [
         {
             title: 'Saved Money for Dinner LandMark',
             subtitle: 'Week in SaiGon',
             currentMoney: 2000000,
             totalMoney: 5200000,
-            date: 'Mar 20, 2024',
+            date: new Date(2024, 2, 23),
             category: require('../assets/icon-food.png')
         },
         {
@@ -16,7 +18,7 @@ export const ChallengeScreen = () => {
             subtitle: 'Week in Vung Tau',
             currentMoney: 2000000,
             totalMoney: 2200000,
-            date: 'Mar 23, 2024',
+            date: new Date(2024, 2, 23),
             category: require('../assets/icon-holiday.png')
         },
         {
@@ -24,12 +26,12 @@ export const ChallengeScreen = () => {
             subtitle: 'Saved Money to win',
             currentMoney: 1800000,
             totalMoney: 4000000,
-            date: 'Feb 20, 2024',
+            date: new Date(2024, 2, 23),
             category: require('../assets/icon-credit.png')
         }
     ]
   return (
-    <Box>
+    <Box flex={1}>
         <Box backgroundColor="blue.700" style={tw`p-5`}>
             <Text bold fontSize='2xl' color="white" style={tw`mb-4`}>Challenge with Friends</Text>
             <View style={tw`flex-row items-center gap-3`}>
@@ -62,12 +64,12 @@ export const ChallengeScreen = () => {
                         <Image source={item.category} alt="Category" style={tw`w-10 h-10`} />
                         <View>
                             <Text bold>{item.title}</Text>
-                            <Text opacity={50}>{item.date}</Text>
+                            <Text opacity={50}>{`${formatter.format(item.date)}, ${item.date.getFullYear()}`}</Text>
                         </View>
                     </View>
                     <View style={tw`flex-row items-center gap-2 mb-3`}>
-                        <Text bold fontSize='2xl'>{`${item.currentMoney.toLocaleString('de-DE')} VNĐ`}</Text>
-                        <Text>{`saved of ${item.totalMoney.toLocaleString('de-DE')} VNĐ`}</Text>
+                        <Text bold fontSize='2xl'>{`${item.currentMoney / 1000000}M VNĐ`}</Text>
+                        <Text>{`saved of ${item.totalMoney / 1000000}M  VNĐ`}</Text>
                     </View>
                     <Progress w="300" shadow={2} value={item.currentMoney / item.totalMoney * 100} style={tw`mb-3`}/>
                     <Text bold>{item.subtitle}</Text>
@@ -75,7 +77,7 @@ export const ChallengeScreen = () => {
             )}
         />
         </ScrollView>
-
+        <Drawer />
     </Box>
   )
 }
