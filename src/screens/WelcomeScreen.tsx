dd53973a6269e0af46073861abcf446db8722301
khanhnguyentuan <khanh.nguyentuan123@hcmut.edu.vn>
2024-04-01 20:41:06 +0700

    test

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -2,28 +2,52 @@ import { Box, View, Text, Button, Image, Pressable } from "native-base"
 import tw from 'twrnc';
 import { useNavigation } from "@react-navigation/native";
 import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from "expo-web-browser";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+
+WebBrowser.maybeCompleteAuthSession();
+const redirectUri = AuthSession.makeRedirectUri();
 export const WelcomeScreen = () => {
     const [userInfo, setUserInfo] = useState(null);
-    const [request, response, prompAsync] = Google.useAuthRequest({
-      androidClientId: "877844441244-ndqdqgbnlmnp6eu65mk20ic03s8uhi40.apps.googleusercontent.com"
+    const [request, response, promptAsync] = Google.useAuthRequest({
+      androidClientId: "877844441244-ndqdqgbnlmnp6eu65mk20ic03s8uhi40.apps.googleusercontent.com",
+      redirectUri
     });
-    const handleGoogleSignIn = async () => {
-      try {
-        const result = await prompAsync();
-        if (result.type === 'success') {
-          // The user is authenticated. You can now use result.params to do things like get the user's name and email.
-          console.log("Nice")
-        } else {
-          // The user did not authenticate successfully. You can handle this however you like in your UI.
-          console.log('User cancelled');
+    useEffect(() => {
+      handleSignInWithGoogle();
+    }, [response]);
+    const handleSignInWithGoogle = async () => {
+      const user = await AsyncStorage.getItem("@user");
+      if (!user) {
+        if(response?.type === "success") {
+          await getUserInfo(response.authentication?.accessToken);
         }
-      } catch (error) {
-        console.error(error);
+      } 
+      else {
+        setUserInfo(JSON.parse(user));
       }
-    };
+    }
+    const getUserInfo = async (token: string | undefined) => {
+      if (!token) return;
+      try {
+        const response = await fetch(
+          "https://www.googleapis.com/userinfo/v2/me",
+          {
+            headers: {Authorization: `Bearer ${token}` }
+          }
+        );
+        const user = await response.json();
+        await AsyncStorage.setItem("@user", JSON.stringify(user));
+        setUserInfo(user);
+      }
+      catch (error) {
+        console.log(error);
+      
+      }
+    }
     const navigation = useNavigation();
   return (
     <Box backgroundColor="white" height='100%'>
@@ -36,15 +60,15 @@ export const WelcomeScreen = () => {
                 Efficient Finance Managemen(EFM)
             </Text>
         <Box style={tw`flex-col items-center gap-3`}>
+            <Text>{userInfo?.name}</Text>
             <Image source={require('../assets/welcome.png')} alt="Welcome" resizeMode="contain" width='50%' height='50%' />
             <Button style={tw`bg-blue-700`} width='90%' onPress={() => navigation.navigate('Login')}>Sign In</Button>
             <Button style={tw`bg-blue-700`} width='90%' onPress={() => navigation.navigate('Signup')}>Sign Up</Button>
-            <Pressable backgroundColor="white" style={tw`flex-row items-center justify-center gap-3 border p-2 rounded`} width='90%' onPress={handleGoogleSignIn}>
+            <Pressable backgroundColor="white" style={tw`flex-row items-center justify-center gap-3 border p-2 rounded`} width='90%' onPress={()=>promptAsync()} >
                 <Image source={require('../assets/google.png')} alt="Google" width={5} height={5} />
                 <Text color="black">Sign in with Google</Text>
             </Pressable>
-            {/* <Button onPress={prompAsync}>Sign In with Google</Button> */}
-        </Box>
+            </Box>
         </Box>
     </Box>
   )
