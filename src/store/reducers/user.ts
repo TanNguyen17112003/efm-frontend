a7db0b5d1f6812bfb2c0f7a5507ce537fa658597
NguyenHung <nguyenhung.220102@gmail.com>
2024-04-29 10:09:56 +0700

    add(redux): friends

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -1,6 +1,9 @@
 import { Config } from 'src/config/api.config';
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axiosInstance from 'src/config/axios.config';
+import { getUserInAsyncStorage } from 'src/helper';
+import { register } from '@services';
 
 type UserRespone = {
   email: string;
@@ -12,8 +15,12 @@ type FetchError = {
   message: any;
 };
 const initialState = {
-  _id: '',
+  id: '',
   email: '',
+  friends: [],
+  requests: [],
+  myRequests: [],
+  allUsers: [],
   name: '',
   loading: false,
   error: ''
@@ -32,7 +39,6 @@ export const getUser = createAsyncThunk<UserRespone, any, { rejectValue: FetchEr
       const response = await axios.get(url, config);
       return response.data;
     } catch (error) {
-      console.log(error);
       return {
         message: 'error'
       };
@@ -40,14 +46,195 @@ export const getUser = createAsyncThunk<UserRespone, any, { rejectValue: FetchEr
   }
 );
 
+export const registerUser = createAsyncThunk(
+  'user/register',
+  async (
+    { email, password, name }: { email: string; password: string; name: string },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await axiosInstance.post('/user/signup', {
+        email,
+        password,
+        name
+      });
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message || 'Failed to register');
+    }
+  }
+);
+
+export const getUserInfo = createAsyncThunk('user/infor', async (_, { rejectWithValue }) => {
+  try {
+    const user = await getUserInAsyncStorage();
+    if (!user || !user.token) {
+      throw new Error('User token not found');
+    }
+    const response = await axiosInstance.get('/user/profile', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    });
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message || 'Failed to register');
+  }
+});
+
+export const getAllUsers = createAsyncThunk('user/all', async (_, { rejectWithValue }) => {
+  try {
+    const user = await getUserInAsyncStorage();
+    if (!user || !user.token) {
+      throw new Error('User token not found');
+    }
+    const response = await axiosInstance.get('/user/all', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    });
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message || 'Failed to register');
+  }
+});
+
+export const sendFriendRequest = createAsyncThunk(
+  'user/sendRequest',
+  async ({ id }: { id: string }, { rejectWithValue }) => {
+    try {
+      const user = await getUserInAsyncStorage();
+      if (!user || !user.token) {
+        throw new Error('User token not found');
+      }
+      const response = await axiosInstance.post(
+        `/user/request/${id}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`
+          }
+        }
+      );
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message || 'Failed to register');
+    }
+  }
+);
+
+export const acceptFriendRequest = createAsyncThunk(
+  'user/acceptRequest',
+  async ({ id }: { id: string }, { rejectWithValue }) => {
+    try {
+      const user = await getUserInAsyncStorage();
+      if (!user || !user.token) {
+        throw new Error('User token not found');
+      }
+      const response = await axiosInstance.put(
+        `/user/request/${id}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`
+          }
+        }
+      );
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message || 'Failed to register');
+    }
+  }
+);
+
+export const rejectRequest = createAsyncThunk(
+  'user/rejectRequest',
+  async ({ id }: { id: string }, { rejectWithValue }) => {
+    try {
+      const user = await getUserInAsyncStorage();
+      if (!user || !user.token) {
+        throw new Error('User token not found');
+      }
+      const response = await axiosInstance.put(
+        `/user/request/${id}/reject`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`
+          }
+        }
+      );
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message || 'Failed to register');
+    }
+  }
+);
+
+export const getAllRequests = createAsyncThunk('user/allRequests', async (_, { rejectWithValue }) => {
+  try {
+    const user = await getUserInAsyncStorage();
+    if (!user || !user.token) {
+      throw new Error('User token not found');
+    }
+    const response = await axiosInstance.get('/user/request', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    });
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message || 'Failed to register');
+  }
+});
+
+export const getAllFriends = createAsyncThunk('user/allFriends', async (_, { rejectWithValue }) => {
+  try {
+    const user = await getUserInAsyncStorage();
+    if (!user || !user.token) {
+      throw new Error('User token not found');
+    }
+    const response = await axiosInstance.get('/user/friends', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    });
+
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message || 'Failed to register');
+  }
+});
+
+export const getAllMyRequests = createAsyncThunk('user/allMyRequest', async (_, { rejectWithValue }) => {
+  try {
+    const user = await getUserInAsyncStorage();
+    if (!user || !user.token) {
+      throw new Error('User token not found');
+    }
+    const response = await axiosInstance.get('user/my-request', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    });
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message || 'Failed to register');
+  }
+});
+
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    _id: '',
+    id: '',
     email: '',
     name: '',
     loading: false,
-    error: ''
+    error: '',
+    friends: [],
+    requests: [],
+    myRequests: [],
+    allUsers: [],
   },
   reducers: {
     logout: (state) => {
@@ -62,14 +249,81 @@ const userSlice = createSlice({
         state.loading = false;
         state.email = payload.email || '';
         state.name = payload.name;
-        state._id = payload._id || '';
+        state.id = payload._id || '';
       }),
       builder.addCase(getUser.rejected, (state, { payload }) => {
         state.loading = false;
         state.error = payload?.message || '';
       });
+    builder.addCase(registerUser.pending, (state) => {
+      state.loading = true;
+    }),
+      builder.addCase(registerUser.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.email = payload.email || '';
+        state.name = payload.name;
+        state.id = payload._id || '';
+      }),
+      builder.addCase(registerUser.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload as string;
+      });
+      builder.addCase(getAllUsers.pending, (state) => {
+        state.loading = true;
+      });
+      builder.addCase(getAllUsers.fulfilled, (state: any, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.allUsers = action.payload;
+      });
+      builder.addCase(getAllUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+      builder.addCase(getAllRequests.pending, (state) => {
+        state.loading = true;
+      });
+      builder.addCase(getAllRequests.fulfilled, (state: any, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.requests = action.payload;
+      });
+      builder.addCase(getAllRequests.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+      builder.addCase(getAllMyRequests.pending, (state) => {
+        state.loading = true;
+      });
+      builder.addCase(getAllMyRequests.fulfilled, (state: any, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.myRequests = action.payload;
+      });
+      builder.addCase(getAllMyRequests.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+      builder.addCase(getAllFriends.pending, (state) => {
+        state.loading = true;
+      });
+      builder.addCase(getAllFriends.fulfilled, (state: any, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.friends = action.payload;
+      });
+      builder.addCase(getAllFriends.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
   }
 });
 
+export const goalsActions = {
+  registerUser,
+  getAllFriends,
+  getAllMyRequests,
+  getAllRequests,
+  getAllUsers,
+  sendFriendRequest,
+  acceptFriendRequest,
+  rejectRequest
+};
 export const { logout } = userSlice.actions;
 export const UserReducers = userSlice.reducer;
